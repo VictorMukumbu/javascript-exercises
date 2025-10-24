e3536ed8503a9ea444cdcf457836caa2cde24a0d
VictorMukumbu <57131903+VictorMukumbu@users.noreply.github.com>
2025-10-24 14:54:38 +0300

    completed tests for reverseString.js

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = string.split("").reverse().join("")
+return result
 };
+reverseString("today")
 
 // Do not edit below this line
 module.exports = reverseString;
