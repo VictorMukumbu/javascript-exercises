c17faa518f9756c5f2388624d4d96af954270529
VictorMukumbu <57131903+VictorMukumbu@users.noreply.github.com>
2025-10-25 11:02:48 +0300

    completed tests for leapYears.js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isYearDivisibleByFour =year%4===0
+    let isYearDivisibleByFourHundred = year%400 ===0
+    let divisibleByHundred = year%100 ===0
+
+if(isYearDivisibleByFour && (isYearDivisibleByFourHundred ||!divisibleByHundred)){
+    return true
+}
+else {return false}
 
 };
 
