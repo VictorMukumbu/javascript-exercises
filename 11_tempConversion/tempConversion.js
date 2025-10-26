e4d45af3d7006eb51c693be70e51cdfd5a6d0272
VictorMukumbu <57131903+VictorMukumbu@users.noreply.github.com>
2025-10-26 14:42:50 +0300

    completed tests for tempConversion.js

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahnherit) {
+  let celcius =(fahnherit-32)*5/9
+  return Number(celcius.toFixed(1))
+
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahnherit = (celcius*9/5)+32
+  return Number(fahnherit.toFixed(1))
 };
 
 // Do not edit below this line
