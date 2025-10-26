59449f197f55c8340990b55f98711cfa8f4bed04
VictorMukumbu <57131903+VictorMukumbu@users.noreply.github.com>
2025-10-26 16:18:59 +0300

    completed tests for calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
+const add = function(num1,num2) {
+  let added =num1+num2
+  return added
 	
 };
 
-const subtract = function() {
+const subtract = function(num1,num2) {
+  let subtracted = num1-num2
+  return subtracted
+
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((accumulator,currentValue)=>accumulator+currentValue,0)
 	
 };
 
-const multiply = function() {
+
+const multiply = function(array) {
+  return array.reduce((accumulator,currentValue)=>accumulator*currentValue,1)
 
 };
 
-const power = function() {
+const power = function(base,exponent) {
+  return Math.pow(base,exponent)
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num===0||num===1){
+    return 1;
+  }
+  else{
+    let result =1
+    for(let i=2;i<=num;i++){
+       result*=i
+    }
+    return result
+  }
 };
 
 // Do not edit below this line
