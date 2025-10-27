de32225ed4964a741f2ca49bf7d061800fb044fc
VictorMukumbu <57131903+VictorMukumbu@users.noreply.github.com>
2025-10-27 07:41:48 +0300

    completed tests for findTheOldest.js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear =new Date().getFullYear()
+
+    return people.reduce((oldestPerson,currentPerson)=>{
+        const oldestAge =(oldestPerson.yearOfDeath??currentYear)-oldestPerson.yearOfBirth
+        const currentAge=(currentPerson.yearOfDeath??currentYear)-currentPerson.yearOfBirth
+
+        return currentAge>oldestAge ?currentPerson:oldestPerson
+
+    })
 
 };
 
